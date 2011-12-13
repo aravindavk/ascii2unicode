a200c2377a0a748024788f303ecf2c1925c1ecc4
Aravinda <hallimanearavind@gmail.com>
2011-12-13 20:44:50 +0530

    Added web version and updated License

--- converter.py
+++ converter.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-# ASCII to Unicode Encoding Converter
-# 
-# Copyright (C) 2011 by Aravinda VK.<hallimanearavind AT gmail.com>,
-# Written by Aravinda <hallimanearavind AT gmail.com>,
-#            Sanchaya <dev AT lists.sanchaya.net>
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-# 
-# This library is distributed in the hope that it will be useful,
+# ASCII2Unicode Kannada Text Encoding converter
+# Copyright (C) 2011  Aravinda VK <hallimanearavind@gmail.com>        
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import os
--- web/map_nudi_baraha.js
+++ web/map_nudi_baraha.js
@@ -1,24 +1,21 @@
-// ASCII to Unicode Encoding Converter
+// ASCII2Unicode Kannada Text Encoding converter
+// Copyright (C) 2011  Aravinda VK <hallimanearavind@gmail.com>        
 
-// Copyright (C) 2011 by Aravinda VK.<hallimanearavind AT gmail.com>,
-// Written by Aravinda <hallimanearavind AT gmail.com>,
-//            Sanchaya <dev AT lists.sanchaya.net>
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-
-// This library is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-exports.mappings = {
+var MapData = {};
+MapData.mappings = {
     "C": "ಅ",
     "D": "ಆ",
     "E": "ಇ",
@@ -238,17 +235,17 @@ exports.mappings = {
     };
 
 //These when joined will be broken as per unicode
-exports.broken_cases = {"Ã":{"value": "ೀ", "mapping": {"ಿ": "ೀ", "ೆ": "ೇ", "ೊ": "ೋ"}}, 
+MapData.broken_cases = {"Ã":{"value": "ೀ", "mapping": {"ಿ": "ೀ", "ೆ": "ೇ", "ೊ": "ೋ"}}, 
                 "Ä":{"value": "ು", "mapping": {}}, 
                 "Æ":{"value": "ೂ", "mapping": {"ೆ":"ೊ"}}, 
                 "È":{"value": "ೃ", "mapping": {}}, 
                 "Ê":{"value": "ೈ", "mapping": {"ೆ":"ೈ"}}  
                 };
 
-exports.dependent_vowels = ["ಾ", "ಿ", "ೀ", "ು", "ೂ", "ೃ", "ೆ", "ೇ", "ೈ", "ೊ", "ೋ", "ೌ"];
-exports.ignore_list = ["ö","÷"];
+MapData.dependent_vowels = ["ಾ", "ಿ", "ೀ", "ು", "ೂ", "ೃ", "ೆ", "ೇ", "ೈ", "ೊ", "ೋ", "ೌ"];
+MapData.ignore_list = ["ö","÷"];
 
-exports.vattaksharagalu = {
+MapData.vattaksharagalu = {
     "Ì": "ಕ",
     "Í": "ಖ",
     "Î": "ಗ",
